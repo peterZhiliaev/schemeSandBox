f3bd1b010a6d366b0da032ba39f24f4798f0d532
peterZhiliaev <118796470+peterZhiliaev@users.noreply.github.com>
2022-11-22 11:30:13 +0300

    Create 01_building_abstraction_with_procedures.scm

---
+++ 01_building_abstraction_with_procedures.scm
@@ -0,0 +1,21 @@
+;; Chapter 1. Building Abstraction with Procedures 
+;; Vocabulary: 
+;; computational process 
+;; data
+;; program 
+;; programming languages 
+;; bugs, glitches 
+;; debug 
+;; interpreter  
+;; expression
+;; evaluating 
+;; Intersting:
+;; Lisp was invented by John McCarthy in the late 1950s 
+;; recursion equations 
+;; LISt Processing
+;; new data object - astoms and lists
+;; second oldest language in widespread use today 
+(display (+ 137 349))
+(newline)
+(define size 2)
+(display (+ size size))
